02cf2c02e95703d0286b7e032f8477e9ba2e49f4
mrhat1993 <mr.hat.1993@gmail.com>
2020-02-01 19:50:11 +0300

    commit

---
+++ Assets/_Assets/_Scripts/Human/HumanPart.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HumanPart : MonoBehaviour
+{
+    [SerializeField] private HumanPartSize _size;
+    [SerializeField] private int _id;
+    [SerializeField] private Transform _direction;
+    public int ID => _id;
+    public HumanPartSize Size => _size;
+    public Transform Direction => _direction;
+}
+
+public enum HumanPartSize
+{
+    Small,
+    Medium,
+    Big
+}
